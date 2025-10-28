43e98a74b440a246911c0c7a63920385d56588e6
uwumai <maicuentaparalau@gmail.com>
2025-10-27 23:35:24 -0500

    cambiosMai

---
+++ app/src/main/java/com/example/iniciodesesion/GeneralFragment.java
@@ -0,0 +1,22 @@
+package com.example.iniciodesesion;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class GeneralFragment extends Fragment {
+
+    public GeneralFragment() {
+        // Constructor vacío requerido
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflar el layout del fragmento
+        return inflater.inflate(R.layout.fragment_general, container, false);
+    }
+}
